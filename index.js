c53775e53dfbc6ed5856a10fe10833b0c3840cbc
Vivek Kumar <kumarvivek79510@gmail.com>
2024-08-17 20:53:40 +0530

    Add static files and 404 error route

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 require('dotenv').config()
 const express = require("express");
+const path = require("path")
 const PORT = process.env.PORT || 3000
 
 const app = express();
 
-app.get('/', (req, res)=>{
-    res.send("<h1>Hello From server of DN Store</h1>")
+app.use(express.urlencoded({extended: false}))
+
+//built in middleware for json
+app.use(express.json());
+
+//serve static files
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('^/$|/index(.html)?', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+app.get('/*', (req, res)=>{
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(PORT, ()=>{
     console.log(`Server started on port ${PORT}`)
 })
+
+
+// Chain Route handler not added. Dave gray Node.js Episode-06
+// Issue to Resolve
+//      Change the route to /error when any error route is requested.
